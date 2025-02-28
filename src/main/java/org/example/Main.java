7260b9814e9b465933cf8d1633e9365a986b1f66
august <august.ryd@gmail.com>
2025-02-28 17:37:22 +0100

    feat: can make softwareDeveloper cover letter

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,24 +2,94 @@ package org.example;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        String templateToEdit = fileEditor();
+        savePdfToDesktop(templateToEdit, "testcompany1");
+    }
 
-        savePdfToDesktop("softwareDeveloper","testcompany1");
+    private static String fileEditor() {
+        String pathToTemplate = selectTemplate();
+        String templateToEdit = getTemplate(pathToTemplate);
 
+        String date = enterDate();
+        templateToEdit = templateToEdit.replace("[Datum]", date);
+
+        String jobTitle = enterJobTitle();
+        templateToEdit = templateToEdit.replace("[Tjänstens namn]", jobTitle);
+
+        String articleNumber = enterArticleNumber();
+        templateToEdit = templateToEdit.replace("[XXXXXX]", articleNumber);
+        return templateToEdit;
+    }
+
+    private static String enterArticleNumber() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter article number: ");
+        return scanner.nextLine();
+    }
+
+    private static String enterJobTitle() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter job title: ");
+        return scanner.nextLine();
+    }
+
+    // todo error handling so you can only enter in the correct format
+    private static String enterDate() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter date: dd/mm/yy: ");
+        return scanner.nextLine();
+    }
+
+    private static String selectTemplate() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("select which CV to print:");
+            System.out.println("1. software developer");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    return "softwareDeveloper";
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 9:
+                    System.out.println("Exiting...");
+                    scanner.close();
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+    }
+
+    private static String getTemplate(String fileToSelect) {
+        try {
+            String filePath = "src/main/resources/" + fileToSelect;
+            String fileContent = Files.readString(Paths.get(filePath));
+            return fileContent;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     // todo replace [Datum] [Tjänstens namn] [XXXXXX]
 
 
-
-    private static void savePdfToDesktop(String file, String companyName) {
+    private static void savePdfToDesktop(String modifiedText, String companyName) {
         try {
-            String filePath = "src/main/resources/" + file;
-            String text = new String(Files.readAllBytes(Paths.get(filePath)));
             PdfMaker pdfMaker = new PdfMaker();
-            pdfMaker.makePdf(text, companyName);
+            pdfMaker.makePdf(modifiedText, companyName);
         } catch (Exception e) {
             e.printStackTrace();
         }
